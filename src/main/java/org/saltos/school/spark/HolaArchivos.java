e730ad2e8fc04e8573a6e3ac250439c092d8c56e
Carlos Saltos <csaltos@gmail.com>
2024-09-17 05:31:08 -0300

    HolaArchivos

--- src/main/java/org/saltos/school/spark/HolaArchivos.java
+++ src/main/java/org/saltos/school/spark/HolaArchivos.java
@@ -1,8 +1,10 @@
 package org.saltos.school.spark;
 
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
 
 public class HolaArchivos {
@@ -23,6 +25,27 @@ public class HolaArchivos {
                 .load("src/main/resources/people.csv");
         peopleCsv.printSchema();
         peopleCsv.show();
+
+        Dataset<Row> peopleTxt = spark.read()
+                .format("csv")
+                .option("sep", ",")
+                .option("header", "false")
+                .option("inferSchema", "false")
+                .load("src/main/resources/people.txt");
+        peopleTxt.printSchema();
+        peopleTxt.show();
+
+        JavaRDD<String> peopleRDD = jsc.textFile("src/main/resources/people.txt");
+        JavaRDD<Row> peopleRowRDD = peopleRDD.map(linea -> {
+            String[] partes = linea.split(",");
+            String nombre = partes[0];
+            String edad = partes[1];
+            Row fila = RowFactory.create(nombre, edad);
+            return fila;
+        });
+
+        System.out.println("People Row RDD:");
+        peopleRowRDD.collect().forEach(System.out::println);
 
         jsc.close();
         spark.close();
