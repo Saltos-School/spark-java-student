4ff45efda072424b16f60534b2d2af1fb639c561
Carlos Saltos <csaltos@gmail.com>
2024-09-19 05:22:12 -0300

    HolaOperaciones

--- src/main/java/org/saltos/school/spark/HolaOperaciones.java
+++ src/main/java/org/saltos/school/spark/HolaOperaciones.java
@@ -19,10 +19,10 @@ public class HolaOperaciones {
         jsc.setLogLevel("WARN");
 
         final List<Double> numeros = Arrays.asList(1.0, 1.1, -5.0, 4.3, 7.98);
-        final JavaRDD<Double> numeroEnSpark = jsc.parallelize(numeros, 10);
+        final JavaRDD<Double> numerosEnSpark = jsc.parallelize(numeros, 10);
 
         // map (transformación)
-        final JavaRDD<Double> incrementoUnoPuntoCinco = numeroEnSpark.map(n -> n + 1.5);
+        final JavaRDD<Double> incrementoUnoPuntoCinco = numerosEnSpark.map(n -> n + 1.5);
 
         // collect (acción)
         incrementoUnoPuntoCinco.collect().forEach(n -> System.out.print(n + " "));
@@ -34,11 +34,25 @@ public class HolaOperaciones {
 
         // flatMap (transformación)
         System.out.println("Por dos y por tres:");
-        //final JavaRDD<List<Double>> porDosYPorTres = numeroEnSpark.map(n -> Arrays.asList(n * 2.0, n * 3.0));
+        //final JavaRDD<List<Double>> porDosYPorTres = numerosEnSpark.map(n -> Arrays.asList(n * 2.0, n * 3.0));
         // JavaDoubleRDD != JavaRDD<Double>
-        final JavaDoubleRDD porDosYPorTres = numeroEnSpark.flatMapToDouble(n -> Arrays.asList(n * 2.0, n * 3.0).iterator());
+        final JavaDoubleRDD porDosYPorTres = numerosEnSpark.flatMapToDouble(n -> Arrays.asList(n * 2.0, n * 3.0).iterator());
         porDosYPorTres.collect().forEach(n -> System.out.print(n + " "));
         System.out.println();
+
+        // reduce (acción)
+        double suma = numerosEnSpark.reduce((x, y) -> x + y);
+        System.out.println("La suma de todos los elementos es: " + suma);
+
+        // map con reduce
+        double sumaDeAbsolutos = numerosEnSpark.map(n -> Math.abs(n)).reduce((x, y) -> x + y);
+        double sumaDeAbsolutos2 = numerosEnSpark.map(Math::abs).reduce(Double::sum);
+        System.out.println("Suma de absolutos es: " + sumaDeAbsolutos);
+        System.out.println("Suma de absolutos 2 es: " + sumaDeAbsolutos2);
+
+        // Dato original
+        System.out.println("Dato original:");
+        numerosEnSpark.collect().forEach(n -> System.out.print(n + " "));
 
         jsc.close();
         spark.close();
