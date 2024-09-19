3c0945ade76df565a88c75b696441f46423bce2f
Carlos Saltos <csaltos@gmail.com>
2024-09-19 06:35:29 -0300

    HolaOperaciones

--- src/main/java/org/saltos/school/spark/HolaOperaciones.java
+++ src/main/java/org/saltos/school/spark/HolaOperaciones.java
@@ -10,6 +10,14 @@ import java.util.List;
 
 public class HolaOperaciones {
 
+    static double getCuadrado(double x) {
+        return x * x;
+    }
+
+    static double sumarDobles(double x, double y) {
+        return x + y;
+    }
+
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder()
                 .appName("HolaEmployees")
@@ -18,7 +26,7 @@ public class HolaOperaciones {
         JavaSparkContext jsc = JavaSparkContext.fromSparkContext(spark.sparkContext());
         jsc.setLogLevel("WARN");
 
-        final List<Double> numeros = Arrays.asList(1.0, 1.1, -5.0, 4.3, 7.98);
+        final List<Double> numeros = Arrays.asList(1.0, 2.0, 3.0);
         final JavaRDD<Double> numerosEnSpark = jsc.parallelize(numeros, 10);
 
         // map (transformación)
@@ -53,6 +61,39 @@ public class HolaOperaciones {
         // Dato original
         System.out.println("Dato original:");
         numerosEnSpark.collect().forEach(n -> System.out.print(n + " "));
+        System.out.println();
+
+        double sumaDeCuadrados = numerosEnSpark.reduce((x, y) -> (x * x) + (y * y));
+        double sumaDeCuadrados2 = numerosEnSpark.map(n -> n * n).reduce((x, y) -> x + y);
+        double sumaDeCuadrados3 = numerosEnSpark.map(n -> Math.pow(n, 2.0)).reduce(Double::sum);
+        double sumaDeCuadrados4 = numerosEnSpark.map(HolaOperaciones::getCuadrado).reduce(HolaOperaciones::sumarDobles);
+        double sumaDeCuadrados5 = numerosEnSpark.mapToDouble(n -> n * n).sum();
+
+        System.out.println("Suma de cuadrados equivocado: " + sumaDeCuadrados);
+        System.out.println("Suma de cuadrados 2: " + sumaDeCuadrados2);
+        System.out.println("Suma de cuadrados 3: " + sumaDeCuadrados3);
+        System.out.println("Suma de cuadrados 4: " + sumaDeCuadrados4);
+        System.out.println("Suma de cuadrados 5: " + sumaDeCuadrados5);
+
+        JavaRDD<Double> porDosYPorTresGenerico = porDosYPorTres.rdd().toJavaRDD();
+        JavaDoubleRDD numerosEnSparkComoNumeroExplito = numerosEnSpark.mapToDouble(n -> n);
+
+        // fold (acción)
+        double sumaConFold = numerosEnSpark.fold(0.0, (x, y) -> x + y);
+        System.out.println("Suma con fold: " + sumaConFold);
+
+        //numerosEnSpark.fold("", (x, y) -> x + " " + y);
+
+        // aggregate (acción)
+
+        String concatenacion = numerosEnSpark.aggregate("", (x, y) -> x + " " + y, (x, y) -> x + y);
+        System.out.println("Concatenación: " + concatenacion);
+
+        String concatenacion2 = numerosEnSpark.aggregate("0.0 + ", (x, y) -> x + "*" + x + " + " + y + "*" + y, (x, y) -> x + " + " + y);
+        System.out.println("Concatenación 2: " + concatenacion2);
+
+        double sumaDeCuadradosAggregate = numerosEnSpark.aggregate(0.0, (x, y) -> (x * x) + (y * y), (x, y) -> x + y);
+        System.out.println("Suma de cuadrados aggregate: " + sumaDeCuadradosAggregate);
 
         jsc.close();
         spark.close();
