9b222e596dfaf0ef84aeb89b842fe4037063bbde
Carlos Saltos <csaltos@gmail.com>
2024-09-20 05:17:58 -0300

    Movies

--- src/main/java/org/saltos/school/spark/Movies.java
+++ src/main/java/org/saltos/school/spark/Movies.java
@@ -4,6 +4,9 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 public class Movies {
 
@@ -14,12 +17,26 @@ public class Movies {
         JavaSparkContext jsc = JavaSparkContext.fromSparkContext(spark.sparkContext());
         jsc.setLogLevel("WARN");
 
-        Dataset<Row> ratingsDF = spark.read().csv("/home/csaltos/Documents/ml-latest-small/ratings.csv");
+        Dataset<Row> ratingsDF = getRatingsDF(spark);
+
         ratingsDF.printSchema();
         ratingsDF.show();
 
-
         jsc.close();
         spark.close();
+    }
+
+    private static Dataset<Row> getRatingsDF(SparkSession spark) {
+        StructType ratingsSchema = DataTypes.createStructType(new StructField[]{
+                        DataTypes.createStructField("userId", DataTypes.LongType, false),
+                        DataTypes.createStructField("movieId", DataTypes.LongType, false),
+                        DataTypes.createStructField("rating", DataTypes.DoubleType, false),
+                        DataTypes.createStructField("timestamp", DataTypes.LongType, false)
+                });
+        Dataset<Row> ratingsDF = spark.read()
+                .option("header", "true")
+                .schema(ratingsSchema)
+                .csv("/home/csaltos/Documents/ml-latest-small/ratings.csv");
+        return ratingsDF;
     }
 }
